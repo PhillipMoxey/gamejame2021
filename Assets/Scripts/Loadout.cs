408414f8e606f412d91bbeea3106f42e4291dd6c
Thomas Kleemann <tkleemann20@gmail.com>
2021-12-11 01:00:34 +1030

    prepping 1 weapon + powerups

--- Assets/Scripts/Loadout.cs
+++ Assets/Scripts/Loadout.cs
@@ -4,47 +4,36 @@ using UnityEngine;
 
 public class Loadout : MonoBehaviour
 {
-    public Weapon primaryWeapon;
-    public Weapon secondaryWeapon;
     public Weapon defaultWeapon;
     public Weapon activeWeapon;
     // Start is called before the first frame update
     void Start()
     {
-        activeWeapon = primaryWeapon;
+        activeWeapon = defaultWeapon;
+        defaultWeapon.unlimitedAmmo = true;
         activeWeapon.spawnLocation = GetComponent<Player>().projSpawnLocation;
-        primaryWeapon.start(gameObject.GetComponent<Player>());
-        secondaryWeapon.start(gameObject.GetComponent<Player>());
-        activeWeapon.weaponImage.gameObject.SetActive(true);
+        if (activeWeapon.weaponImage)
+            activeWeapon.weaponImage.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
         activeWeapon.update();
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            activeWeapon.weaponImage.gameObject.SetActive(false);
-            if (activeWeapon == primaryWeapon)
-                activeWeapon = secondaryWeapon;
-            else if (activeWeapon == secondaryWeapon)
-                activeWeapon = primaryWeapon;
-            activeWeapon.weaponImage.gameObject.SetActive(true);
-            UpdateUI();
-        }
-    }
-
-    public void SwapWeapon(Weapon weapon)
-    {
-        secondaryWeapon = weapon;
     }
 
     public void UpdateUI()
     {
-        UIManager.instance.ammoCount.text = activeWeapon.ammoCount.ToString();
+
         if (!activeWeapon.unlimitedAmmo)
+        {
+            UIManager.instance.ammoCount.text = activeWeapon.ammoCount.ToString();
             UIManager.instance.totalAmmo.text = activeWeapon.totalAmmo.ToString();
+        }
         else
+        {
+            UIManager.instance.ammoCount.text = "999";
             UIManager.instance.totalAmmo.text = "999";
+        }
     }
 }
